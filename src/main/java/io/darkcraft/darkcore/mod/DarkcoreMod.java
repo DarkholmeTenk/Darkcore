3c5a93886f8600617ccfb4d7327083f54f0702bd
DarkholmeTenk <dark@darkhol.me>
2015-02-24 12:19:51 +0000

    Initial commit.

---
+++ src/main/java/io/darkcraft/darkcore/mod/DarkcoreMod.java
@@ -0,0 +1,9 @@
+package io.darkcraft.darkcore.mod;
+
+import cpw.mods.fml.common.Mod;
+
+@Mod(modid="darkcore", version="0.1")
+public class DarkcoreMod
+{
+
+}
---
+++ src/main/java/io/darkcraft/darkcraft/mod/DarkcraftMod.java
@@ -0,0 +1,15 @@
+package io.darkcraft.darkcraft.mod;
+
+import cpw.mods.fml.common.Mod;
+import cpw.mods.fml.common.Mod.EventHandler;
+import cpw.mods.fml.common.event.FMLPreInitializationEvent;
+
+@Mod(modid="darkcraft", version="0.1", dependencies="required-after:darkcore")
+public class DarkcraftMod
+{
+	@EventHandler
+	public void preInit(FMLPreInitializationEvent event)
+	{
+		
+	}
+}
