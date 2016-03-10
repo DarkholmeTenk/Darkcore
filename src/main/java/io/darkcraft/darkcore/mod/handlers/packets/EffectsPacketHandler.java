b4286aa0552e5d53c0c16d737be28184899fc040
DarkholmeTenk <shaney.booth@gmail.com>
2016-03-10 20:17:22 +0000

    Fixes crash bug

--- src/main/java/io/darkcraft/darkcore/mod/handlers/packets/EffectsPacketHandler.java
+++ src/main/java/io/darkcraft/darkcore/mod/handlers/packets/EffectsPacketHandler.java
@@ -1,17 +1,26 @@
 package io.darkcraft.darkcore.mod.handlers.packets;
 
 import io.darkcraft.darkcore.mod.handlers.EffectHandler;
+import io.darkcraft.darkcore.mod.helpers.ServerHelper;
 import io.darkcraft.darkcore.mod.impl.EntityEffectStore;
 import io.darkcraft.darkcore.mod.interfaces.IDataPacketHandler;
 import net.minecraft.client.Minecraft;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.nbt.NBTTagCompound;
+import cpw.mods.fml.relauncher.Side;
+import cpw.mods.fml.relauncher.SideOnly;
 
 public class EffectsPacketHandler implements IDataPacketHandler
 {
 	public static final byte effPacketDisc = 4;
 	@Override
 	public void handleData(NBTTagCompound data)
+	{
+		if(ServerHelper.isClient()) handleDataC(data);
+	}
+
+	@SideOnly(Side.CLIENT)
+	private void handleDataC(NBTTagCompound data)
 	{
 		if(!data.hasKey("dcEff")) return;
 		String type = data.getString("dcEff");
