f70eb4b1e70d8d37c15065034323c5dd48431e71
DarkholmeTenk <shaney.booth@gmail.com>
2016-04-08 08:25:36 +0100

    Code for rendering structures

--- src/main/java/io/darkcraft/darkcore/mod/helpers/RenderHelper.java
+++ src/main/java/io/darkcraft/darkcore/mod/helpers/RenderHelper.java
@@ -1,14 +1,23 @@
 package io.darkcraft.darkcore.mod.helpers;
 
 import io.darkcraft.darkcore.mod.datastore.UVStore;
+import io.darkcraft.darkcore.mod.multiblock.IBlockState;
+import io.darkcraft.darkcore.mod.multiblock.IMultiBlockStructure;
+import io.darkcraft.darkcore.mod.multiblock.RenderBlockAccess;
+import net.minecraft.block.Block;
 import net.minecraft.client.Minecraft;
+import net.minecraft.client.renderer.RenderBlocks;
 import net.minecraft.client.renderer.Tessellator;
+import net.minecraft.client.renderer.tileentity.TileEntityRendererDispatcher;
+import net.minecraft.tileentity.TileEntity;
 import net.minecraft.util.ResourceLocation;
 
 import org.lwjgl.Sys;
+import org.lwjgl.opengl.GL11;
 
 public class RenderHelper
 {
+	private static RenderBlocks rb;
 	public static void bindTexture(ResourceLocation rl)
 	{
 		Minecraft.getMinecraft().renderEngine.bindTexture(rl);
@@ -43,5 +52,52 @@ public class RenderHelper
 	public static long getTime()
 	{
 		return (Sys.getTime() * 1000) / Sys.getTimerResolution();
+	}
+
+	public static void renderMultiBlock(IMultiBlockStructure struct, RenderBlockAccess rba)
+	{
+		GL11.glPushMatrix();
+		IBlockState[][][] s = struct.getStructureDefinition();
+		rb.blockAccess=rba;
+		Tessellator.instance.startDrawingQuads();
+		RenderHelper.bindTexture(new ResourceLocation("textures/atlas/blocks.png"));
+		for(int y = 0; y < s.length; y++)
+		{
+			IBlockState[][] fl = s[y];
+			if(fl == null) continue;
+			for(int x = 0; x <fl.length; x++)
+			{
+				IBlockState[] row = fl[x];
+				if(row == null) continue;
+				for(int z = 0; z < row.length; z++)
+				{
+					IBlockState bs = row[z];
+					if(bs == null) continue;
+					Block b = bs.getDefaultBlock();
+					if(b == null) continue;
+					rb.renderBlockByRenderType(b, x, y, z);
+				}
+			}
+		}
+		Tessellator.instance.draw();
+		for(int y = 0; y < s.length; y++)
+		{
+			IBlockState[][] fl = s[y];
+			if(fl == null) continue;
+			for(int x = 0; x <fl.length; x++)
+			{
+				IBlockState[] row = fl[x];
+				if(row == null) continue;
+				for(int z = 0; z < row.length; z++)
+				{
+					IBlockState bs = row[z];
+					if(bs == null) continue;
+					TileEntity te = rb.blockAccess.getTileEntity(x, y, z);
+					if((te != null) && TileEntityRendererDispatcher.instance.hasSpecialRenderer(te))
+						TileEntityRendererDispatcher.instance.renderTileEntityAt(te, x, y, z, 0);
+				}
+			}
+		}
+		GL11.glPopMatrix();
 	}
 }
