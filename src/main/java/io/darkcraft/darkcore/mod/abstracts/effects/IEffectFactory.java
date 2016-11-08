474b22b99e8cad00e7409ea88213483a1c576913
DarkholmeTenk <shaneybooth@gmail.com>
2016-11-08 14:14:56 +0000

    Ripped everything apart

--- src/main/java/io/darkcraft/darkcore/mod/abstracts/effects/IEffectFactory.java
+++
@@ -1,9 +0,0 @@
-package io.darkcraft.darkcore.mod.abstracts.effects;
-
-import net.minecraft.entity.EntityLivingBase;
-import net.minecraft.nbt.NBTTagCompound;
-
-public interface IEffectFactory
-{
-	public AbstractEffect createEffect(EntityLivingBase ent, String id, NBTTagCompound nbt);
-}
--- src/main/java/io/darkcraft/darkcore/mod/multiblock/IMultiBlockPart.java
+++
@@ -1,8 +0,0 @@
-package io.darkcraft.darkcore.mod.multiblock;
-
-public interface IMultiBlockPart
-{
-	public void setMultiBlockCore(IMultiBlockCore core);
-
-	public void recheckCore();
-}
