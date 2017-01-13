5de60f530594c8cda136ee392ab86a42cbc60a40
DarkholmeTenk <shaney.booth@gmail.com>
2017-01-13 13:37:04 +0000

    Added ability to have NBTMethods

---
+++ src/main/java/io/darkcraft/darkcore/mod/nbt/NBTMethod.java
@@ -0,0 +1,20 @@
+package io.darkcraft.darkcore.mod.nbt;
+
+import static java.lang.annotation.ElementType.METHOD;
+import static java.lang.annotation.RetentionPolicy.RUNTIME;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.Target;
+
+@Retention(RUNTIME)
+@Target(METHOD)
+public @interface NBTMethod
+{
+	Type value();
+
+	public static enum Type
+	{
+		READ,
+		WRITE;
+	}
+}
--- src/main/java/io/darkcraft/darkcore/mod/nbt/impl/GeneratedMapper.java
+++ src/main/java/io/darkcraft/darkcore/mod/nbt/impl/GeneratedMapper.java
@@ -3,33 +3,87 @@ package io.darkcraft.darkcore.mod.nbt.impl;
 import java.lang.reflect.Constructor;
 import java.lang.reflect.Field;
 import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 import java.lang.reflect.Modifier;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.EnumMap;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Map;
 import java.util.Map.Entry;
 
+import com.google.common.base.Supplier;
 import com.google.common.collect.Maps;
+import com.google.common.collect.Multimap;
+import com.google.common.collect.Multimaps;
+
+import net.minecraft.nbt.NBTTagCompound;
 
 import io.darkcraft.darkcore.mod.nbt.Mapper;
 import io.darkcraft.darkcore.mod.nbt.NBTConstructor;
 import io.darkcraft.darkcore.mod.nbt.NBTHelper;
+import io.darkcraft.darkcore.mod.nbt.NBTMethod;
+import io.darkcraft.darkcore.mod.nbt.NBTMethod.Type;
 import io.darkcraft.darkcore.mod.nbt.NBTProperty;
 import io.darkcraft.darkcore.mod.nbt.NBTProperty.SerialisableType;
 import io.darkcraft.darkcore.mod.nbt.NBTSerialisable;
-import net.minecraft.nbt.NBTTagCompound;
 
 public class GeneratedMapper<T> extends Mapper<T>
 {
+
+	private static <T> NBTConstructorData getConstructor(Class<T> clazz, Map<Field, Data> fields)
+	{
+		try
+		{
+			for(Constructor<?> constrObj : clazz.getConstructors())
+			{
+				Constructor<T> constr = (Constructor<T>) constrObj;
+				NBTConstructor annotation = constr.getAnnotation(NBTConstructor.class);
+				if(annotation != null)
+				{
+					return new NBTConstructorData(annotation, constr, fields);
+				}
+			}
+		}
+		catch(SecurityException e){
+			e.printStackTrace();
+		}
+		return null;
+	}
+
+	private static <T> Multimap<Type, Method> getMethods(Class<T> clazz)
+	{
+		Multimap<Type, Method> methods = Multimaps.newSetMultimap(new EnumMap<Type, Collection<Method>>(Type.class),
+				new Supplier<HashSet<Method>>()
+				{
+					@Override
+					public HashSet<Method> get()
+					{
+						return new HashSet<>();
+					}
+				});
+		for(Method m : clazz.getMethods())
+		{
+			NBTMethod annotation = m.getAnnotation(NBTMethod.class);
+			if(annotation == null)
+				continue;
+			if(!Arrays.equals(m.getParameterTypes(), new Class<?>[]{NBTTagCompound.class}))
+				throw new RuntimeException("Exception constructing mapper. " + m.getName() + " does not take a single NBTTagCompound");
+			methods.put(annotation.value(), m);
+		}
+		return methods;
+	}
 
 	@SuppressWarnings({ "unchecked", "rawtypes" })
 	public static <T> Mapper<T> getMapper(Class<T> clazz, SerialisableType t)
 	{
 		if(NBTHelper.hasMapper(clazz, t))
-			return (Mapper<T>) NBTHelper.getMapper(clazz, t);
+			return NBTHelper.getMapper(clazz, t);
 		NBTSerialisable serProp = clazz.getAnnotation(NBTSerialisable.class);
 		if(serProp == null)
 			return null;
-		Mapper<? super T> parent = (Mapper<? super T>) NBTHelper.getMapper(clazz.getSuperclass(), t);
+		Mapper<? super T> parent = NBTHelper.getMapper(clazz.getSuperclass(), t);
 
 		Map<Field, Data> fields;
 		if(parent instanceof GeneratedMapper)
@@ -50,24 +104,9 @@ public class GeneratedMapper<T> extends Mapper<T>
 			if(!serProp.createNew() && Modifier.isFinal(f.getModifiers()))
 				throw new RuntimeException("Field " + f.getName() + " is final, but class is not set to create new instances on fill");
 		}
-		NBTConstructorData<T> constructor = null;
-		try
-		{
-			for(Constructor<?> constrObj : clazz.getConstructors())
-			{
-				Constructor<T> constr = (Constructor<T>) constrObj;
-				NBTConstructor annotation = constr.getAnnotation(NBTConstructor.class);
-				if(annotation != null)
-				{
-					constructor = new NBTConstructorData(annotation, constr, fields);
-					break;
-				}
-			}
-		}
-		catch(SecurityException e){
-			e.printStackTrace();
-		}
-		Mapper<T> m = new GeneratedMapper(clazz, fields, constructor, serProp.createNew());
+		NBTConstructorData<T> constructor = getConstructor(clazz, fields);
+		Multimap<Type, Method> methods = getMethods(clazz);
+		Mapper<T> m = new GeneratedMapper(clazz, fields, methods, constructor, serProp.createNew());
 		if(serProp.includeType())
 			m = new TypeWrapperMapper(clazz, m);
 		return m;
@@ -76,16 +115,18 @@ public class GeneratedMapper<T> extends Mapper<T>
 	private final Class<T> clazz;
 	private final Map<Field, Data> fields;
 	private final Map<String, Data> fieldNames;
+	private final Multimap<Type, Method> methods;
 	private final NBTConstructorData<T> constructor;
 	private final boolean shouldCreateNew;
 
-	private GeneratedMapper(Class c, Map<Field, Data> fieldMap, NBTConstructorData<T> constructor, boolean shouldCreateNew)
+	private GeneratedMapper(Class c, Map<Field, Data> fieldMap, Multimap<Type, Method> methods, NBTConstructorData<T> constructor, boolean shouldCreateNew)
 	{
 		clazz = c;
 		fields = fieldMap;
 		fieldNames = new HashMap<>();
 		for(Entry<Field, Data> entry : fields.entrySet())
 			fieldNames.put(entry.getKey().getName(), entry.getValue());
+		this.methods = methods;
 		this.constructor = constructor;
 		this.shouldCreateNew = shouldCreateNew;
 	}
@@ -106,6 +147,14 @@ public class GeneratedMapper<T> extends Mapper<T>
 					m.writeToNBT(nbt, n, o);
 			}
 			catch(SecurityException | IllegalArgumentException | IllegalAccessException e){}
+		}
+		for(Method m : methods.get(Type.WRITE))
+		{
+			try
+			{
+				m.invoke(t, nbt);
+			}
+			catch (SecurityException | IllegalArgumentException | IllegalAccessException | InvocationTargetException e){}
 		}
 	}
 
@@ -138,6 +187,14 @@ public class GeneratedMapper<T> extends Mapper<T>
 					f.set(t, null);
 			}
 			catch(SecurityException | IllegalArgumentException | IllegalAccessException e){}
+		}
+		for(Method m : methods.get(Type.READ))
+		{
+			try
+			{
+				m.invoke(t, nbt);
+			}
+			catch (SecurityException | IllegalArgumentException | IllegalAccessException | InvocationTargetException e){}
 		}
 		return t;
 	}
